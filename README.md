37de561727aa16fb9c6e0533034c3363b6372fb2
cloudinactionorg <cloudinactionorg@gmail.com>
2023-05-15 21:38:09 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tf-tutorials
+
+List of Terraform tutorials from Hashicorp web site
---
+++ doker/learn-terraform-docker-container/variables.tf
@@ -0,0 +1,5 @@
+variable "container_name" {
+  description = "Value of the name of Docker container"
+  type = string
+  default = "ExampleNGContainer"
+}
